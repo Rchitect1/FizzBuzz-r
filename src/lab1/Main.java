dccfd78c82f5e43b0bd531d333d6b35ac4437a35
Reza Homayounmehr <rezahm@Rezas-MacBook-Pro.local>
2025-09-08 16:27:57 -0400

    second set of commits

---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] str = new String[0];
+        FizzBuzz.main(str);
+        FizzBuzz.doFizzbuzz(5);
+    }
+}
